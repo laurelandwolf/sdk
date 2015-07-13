474625c64e369c6303c913bec53256cddaf31a78
Michael Graham <michael@endlessdoloop.com>
2015-07-13 12:24:06 -0700

    Better specs

--- test/api.js
+++ test/api.js
@@ -3,46 +3,18 @@ import api from '../src/api';
 
 let test = namespace('api');
 
-test('projects', ({equal}) => {
+let resources = ['Project', 'Designer', 'Room', 'Recipient'];
 
-  let a = api();
+resources.forEach((resource) => {
 
-  equal(typeof a.getProjects, 'function', 'GET projects');
-  equal(typeof a.getProject, 'function', 'GET projects/:id');
-  equal(typeof a.createProject, 'function', 'POST projects');
-  equal(typeof a.updateProject, 'function', 'PATCH projects/:id');
-  equal(typeof a.deleteProject, 'function', 'DELETE projects/:id');
-});
+  test(resource, ({equal}) => {
 
-test('designers', ({equal}) => {
+    let a = api();
 
-  let a = api();
-
-  equal(typeof a.getDesigners, 'function', 'GET designers');
-  equal(typeof a.getDesigner, 'function', 'GET designers/:id');
-  equal(typeof a.createDesigner, 'function', 'POST designers');
-  equal(typeof a.updateDesigner, 'function', 'PATCH designers/:id');
-  equal(typeof a.deleteDesigner, 'function', 'DELETE designers/:id');
-});
-
-test('rooms', ({equal}) => {
-
-  let a = api();
-
-  equal(typeof a.getRooms, 'function', 'GET rooms');
-  equal(typeof a.getRoom, 'function', 'GET rooms/:id');
-  equal(typeof a.createRoom, 'function', 'POST rooms');
-  equal(typeof a.updateRoom, 'function', 'PATCH rooms/:id');
-  equal(typeof a.deleteRoom, 'function', 'DELETE rooms/:id');
-});
-
-test('recipients', ({equal}) => {
-
-  let a = api();
-
-  equal(typeof a.getRecipients, 'function', 'GET recipients');
-  equal(typeof a.getRecipient, 'function', 'GET recipients/:id');
-  equal(typeof a.createRecipient, 'function', 'POST recipients');
-  equal(typeof a.updateRecipient, 'function', 'PATCH recipients/:id')
-  equal(typeof a.deleteRecipient, 'function', 'DELETE recipients/:id')
+    equal(typeof a[`get${resource}s`], 'function', `GET ${resource}s`);
+    equal(typeof a[`get${resource}`], 'function', `GET ${resource}s/:id`);
+    equal(typeof a[`create${resource}`], 'function', `POST ${resource}s`);
+    equal(typeof a[`update${resource}`], 'function', `PATCH ${resource}s/:id`);
+    equal(typeof a[`delete${resource}`], 'function', `DELETE ${resource}s/:id`);
+  });
 });
